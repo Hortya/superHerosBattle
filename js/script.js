800ab7af7601b67fc92d7193ca06907a11cae5c6
Hortense Chambron <hortense.chambron@gmail.com>
2024-04-22 14:51:16 +0200

    save images display

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
-fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
-    .then(response => response.json())
-    .then(json => {localStorage.setItem('herosSheets', JSON.stringify(json))})
+function displayCharacters(allHeroesSheets) {
+    ul = document.getElementById('allSup')
+    allHeroesSheets.forEach(herosObject => {
+        const li = document.createElement('li');
+        li.innerHTML = `<img src="${herosObject.images.sm}"/> <button id=${herosObject.id} class='button js-btn'>${herosObject.name}</button>`
+        ul.appendChild(li)
+    }); 
+}
+
+
+async function battleRoyal() {
+    try {
+        const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
+        const allHeroesSheets = await response.json();
+
+        displayCharacters(allHeroesSheets);
+
+
+        ul.addEventListener('click', function (event) {
+            console.log(event);
+            if (!event.target.classList.contains('js-btn')) return;
+
+            console.log("hello");
+        })
+
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+
+battleRoyal()
+
+
 
