5fe62fe752bd7752fd838e450deb1292cfbec314
Hortense Chambron <hortense.chambron@gmail.com>
2024-04-22 13:36:50 +0200

    api  on page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log('bjr');
+fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
+    .then(response => response.json())
+    .then(json => {localStorage.setItem('herosSheets', JSON.stringify(json))})
+
