3242f50594374b99f58555112c580130d775e1cf
Hortense Chambron <hortense.chambron@gmail.com>
2024-04-22 16:38:03 +0200

    save add and remove personnage

--- js/script.js
+++ js/script.js
@@ -2,32 +2,72 @@ function displayCharacters(allHeroesSheets) {
     ul = document.getElementById('allSup')
     allHeroesSheets.forEach(herosObject => {
         const li = document.createElement('li');
-        li.innerHTML = `<img src="${herosObject.images.sm}"/> <button id=${herosObject.id} class='button js-btn'>${herosObject.name}</button>`
+        li.innerHTML = `<img src="${herosObject.images.sm}"/> <button data-id=${herosObject.id} class='button js-btn'>${herosObject.name}</button>`
         ul.appendChild(li)
-    }); 
+    });
 }
+
+// function puttInTheArray(event, array) {
+//     let isInTheArray;
+
+//     for (const id of array) {
+//         if (id === event.target.dataset.id) {
+
+//             isInTheArray = true;
+//         };
+//     }
+
+//     if (isInTheArray === true) {
+//         alert("Vous ne pouvez pas selectionner le même personnage");
+//         return
+//     }
+
+//     array.push(event.target.dataset.id);
+// }
+
+function addOnClick(array) {
+    ul.addEventListener('click', function (event) {
+        if (!event.target.classList.contains('js-btn')) return;
+            const li = document.createElement('li');
+            li.innerHTML = event.target.parentNode.innerHTML
+            document.getElementById('selectedSup').appendChild(li)
+            event.target.parentNode.remove()
+            // puttInTheArray(event, array)
+
+            console.log(array);
+        })
+    }
+
+
+async function MakeHerosArray() {
+            try {
+                const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
+                return await response.json();
+            }
+            catch (e) {
+                console.error(e)
+            }
+        }
+    
 
 
 async function battleRoyal() {
-    try {
-        const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
-        const allHeroesSheets = await response.json();
+            try {
+                const allHeroesSheets = await MakeHerosArray();
+                displayCharacters(allHeroesSheets);
+                let fighters = [];
 
-        displayCharacters(allHeroesSheets);
+                addOnClick(fighters);
 
 
-        ul.addEventListener('click', function (event) {
-            console.log(event);
-            if (!event.target.classList.contains('js-btn')) return;
+                // document.getElementById('battle').addEventListener('click', function(event, fighters){
 
-            console.log("hello");
-        })
-
-    }
-    catch (e) {
-        console.error(e)
-    }
-}
+                // })
+            }
+            catch (e) {
+                console.error(e)
+            }
+        }
 
 battleRoyal()
 
