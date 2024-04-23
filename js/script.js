c380b6a7a834d712b17d9014e3d05dafd0034987
Hortense Chambron <hortense.chambron@gmail.com>
2024-04-23 14:56:22 +0200

    template done

--- js/script.js
+++ js/script.js
@@ -6,8 +6,14 @@ function displayCharacters(allHeroesSheets) {
         const herosList = document.getElementById('supItm');
         const hero = document.importNode(herosList.content, true);
         hero.querySelector('.js-sup-name').textContent = herosObject.name;
+        hero.querySelector('.js-publisher').textContent = herosObject.biography.publisher;
+        hero.querySelector('.js-alignment').textContent = herosObject.biography.alignment;
+        hero.querySelector('.js-intel').textContent = herosObject.powerstats.intelligence;
+        hero.querySelector('.js-strength').textContent = herosObject.powerstats.strength;
+        hero.querySelector('.js-speed').textContent = herosObject.powerstats.speed;
         hero.querySelector('.js-sup-img').src = herosObject.images.sm;
         hero.querySelector('.js-sup-img').alt = `photo de ${herosObject.name}`
+        hero.querySelector('.js-btn').setAttribute('data-id', herosObject.id)
         ul.appendChild(hero)
     });
 }
@@ -36,8 +42,8 @@ function addOnClick(array, fighters) {
     ul.addEventListener('click', function (event) {
         if (!event.target.classList.contains('js-btn')) return;
         const li = document.createElement('li');
-        li.innerHTML = event.target.parentNode.innerHTML
-        document.getElementById('selectedSup').appendChild(li)
+        li.innerHTML = event.target.parentNode.innerHTML;
+        document.getElementById('selectedSup').appendChild(li);
         fighters.push(array.filter((heros) => heros.id == event.target.dataset.id))
         event.target.parentNode.remove()
         console.log(fighters);
@@ -74,8 +80,8 @@ async function battleRoyal() {
         let fighters = [];
 
         addOnClick(allHeroesSheets, fighters);
-        removeOnClick(fighters);
-
+        // removeOnClick(fighters);
+        battleStart(fighters)
 
         // document.getElementById('battle').addEventListener('click', function(event, fighters){
 
@@ -91,7 +97,6 @@ async function battleRoyal() {
 function battleStart(fighters) {
     document.getElementById('battle').addEventListener('click', () => {
         
-
     })
 }
 
@@ -104,3 +109,17 @@ battleRoyal()
 
 
 
+
+// const ulBattle = document.createElement('ul');
+// ulBattle.classList.add('battle');
+// ulBattle.id = 'battleTemplate';
+// document.body.appendChild(ulBattle)
+// fighters.forEach((fighter) => {
+//     const herosList = document.getElementById('battleItm');
+//     const hero = document.importNode(herosList.content, true);
+//     hero.querySelector('.js-sup-name').textContent = fighter[0].name;
+//     hero.querySelector('.js-sup-img').src = fighter[0].images.sm;
+//     hero.querySelector('.js-sup-img').alt = `photo de ${fighter[0].name}`;
+//     hero.querySelector('.js-sup-info').textContent = fighter[0].powerstats;
+//     document.getElementById('battleTemplate').appendChild(hero);
+// })
